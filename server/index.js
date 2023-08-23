17efe2e70f318766ed294080e2ad9c861af10408
Tom Warner <tommwarner@gmail.com>
2023-08-23 19:48:37 -0400

    working framework

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const app = express();
+const app = require('./app');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
